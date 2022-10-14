af787397dc70d734acc698af8a416eb4454258f6
Alejandro <alosada@gmail.com>
2022-10-14 14:32:54 -0400

    first commit

---
+++ lib/flipgive_skd.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "flipgive_skd/version"
+
+module FlipgiveSkd
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/flipgive_skd/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module FlipgiveSkd
+  VERSION = "0.1.0"
+end
